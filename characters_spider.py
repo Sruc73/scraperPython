26f98a25426c669ba765768d4b3a659227703a9e
Sruc73 <michaelmares@outlook.com>
2017-06-21 10:48:03 +0200

    Changed class name

--- characters_spider.py
+++ characters_spider.py
@@ -1,14 +1,14 @@
 import scrapy
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+class BlogSpider(scrapy.Spider):
+    name = "blogspider"
     start_urls = [
         'https://fr.wikipedia.org/wiki/Cat%C3%A9gorie:Personnage_d%27animation',
     ]
 
     def parse(self, response):
-        for quote in response.css('div#mw-pages div.mw-content-ltr li'):
+        for character in response.css('div#mw-pages div.mw-content-ltr li'):
             yield {
                 'character': quote.css('a ::text').extract_first(),
             }
